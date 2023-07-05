bc91d6685bda01f1ba5a26f104398f7ecbe408b9
= <=>
2023-07-05 18:32:25 -0300

    falta ajeitar as info das tarefas

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -5,7 +5,17 @@ const Tasks = ({ list }) => {
   return (
     <>
       {list.map((item) => {
-        return <p key={item.id}>{item.name}</p>;
+        return (
+          <section key={item.id} className='boxItemTask'>
+            <p className='nameTask' key={item.id}>
+              {item.name}
+            </p>
+            <div className='boxDataTask'>
+              <p>14:12</p>
+              <p>22/09</p>
+            </div>
+          </section>
+        );
       })}
     </>
   );
