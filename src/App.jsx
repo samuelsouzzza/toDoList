32dd6b64aacf1e2dda4241db1f48860604c482d9
= <=>
2023-07-05 15:37:03 -0300

    ajustes no css do input e seu btn

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,15 @@ const App = () => {
       <section className='card'>
         <div className='boxTasks'>
           <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
-          <Tasks />
         </div>
-        <div className='boxInput'>sadf</div>
+        <div className='boxInput'>
+          <input
+            type='text'
+            placeholder='O que você precisa fazer hoje?'
+            className='inputTask'
+          />
+          <button className='btnSaveTask'>SALVAR</button>
+        </div>
       </section>
     </div>
   );
